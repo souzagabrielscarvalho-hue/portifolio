5a7f03d2f348db34d49318944e9b8bd3b1735ae1
Gabriel Carvalho <souza.gabrielscarvalho@gmail.com>
2026-03-18 06:39:15 -0300

    Atualiza requirements com versões compatíveisssssss

--- app/main.py
+++ app/main.py
@@ -7,12 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
-app = FastAPI(title="API do Meu Portfólio IA")
-# app = FastAPI(
-#     title="API do Meu Portfólio IA", 
-#     description="Uma API em 3 camadas consultando PDFs via AWS Bedrock",
-#     lifespan=lifespan
-# )
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,8 +15,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(router)
-
+app.include_router(router, prefix="/api")
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
